52455549c87ed3385e3c00bc2d263a17a4845b4f
gfarrasb <gfarrasb@gmail.com>
2020-08-11 11:27:42 +0200

    Subida del proyecto. Desarrollado en local. Curso de Coursera de Angular

---
+++ Projects/angular/angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -0,0 +1,11 @@
+export class DestinoViaje {
+	nombre:string;
+ 	imagenUrl:string;
+ 	descripcion:string;
+
+	constructor(n:string, u:string, d:string) {
+		this.nombre=n;
+		this.imagenUrl=u;
+		this.descripcion=d;
+	}
+}
